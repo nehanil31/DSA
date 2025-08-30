a6b73aa18239601838670464514f503f63319741
NEHA ANIL <165749232+nehanil31@users.noreply.github.com>
2025-08-30 11:32:41 +0530

    Add files via upload

---
+++ set_matrix_zeroes.py
@@ -0,0 +1,56 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        """
+        def markRow(i):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]!=0:
+                    matrix[i][j]=-1
+        def markColumn(j):
+            for i in range(len(matrix)):
+                if matrix[i][j]!=0:
+                    matrix[i][j]=-1            
+
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==0:
+                    markRow(i)
+                    markColumn(j)
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==-1:
+                    matrix[i][j]=0
+        return matrix """
+        col0=1
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==0:
+                    matrix[i][0]=0
+                    if j!=0:
+                        matrix[0][j]=0
+                    else:
+                        col0=0
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[0])):
+                if matrix[i][j]!=0:
+                    if matrix[i][0] ==0 or matrix[0][j]==0:
+                        matrix[i][j]=0
+        if matrix[0][0]==0:
+            for j in range(len(matrix[0])):
+                matrix[0][j]=0
+        if col0==0:
+            for i in range(len(matrix)):
+                matrix[i][0]=0  
+
+        return              
+
+                        
+
+                    
+
+
+        
